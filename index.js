8420af655b2900ff8e9c7d40f8422f2da3b09825
luizsilva <luizfsilva.091595@gmail.com>
2021-07-15 10:47:24 -0400

    connected to Mongo

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express')       // import express
 const cors = require('cors')             // import cors
 const mongoose = require('mongoose')     // import mongoose
+require('dotenv/config')                 // importing dotenv library to use variable
 
 const app = express()                    // creating app as Express
 app.use(express.json())                  // use express and parse everything into json
 
-// connect to mongoose
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})    // connect to mongoose
+.then(() => console.log('connected to Mongo...'))
+.catch(err => console.log(err))
+
 app.get('/', (req, res)=>{
     res.send('Hello')
 })
 
 app.listen(5000, (req, res) => {
-    console.log('Listening on port 5000...')
+    console.log('Listening on http://localhost:5000/...')
 })
