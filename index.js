8c43840d72ee85968a9044d21ae62e7a0f068697
luizsilva <luizfsilva.091595@gmail.com>
2021-07-15 16:10:46 -0400

    cleaned up

--- index.js
+++ index.js
@@ -17,26 +17,3 @@ mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTo
 // import the router
 const eventsRoutes = require('./src/routes/eventsRoutes')
 app.use(eventsRoutes)
-
-
-
-// app.get('/', (req, res)=>{
-//     createEvent()
-//     res.send('Event was created successfully')
-// })
-
-
-// const newEvent = {
-//     title: 'Clean up Boca Beach',
-//     date: '2021-08-02',
-//     description: 'Cleaning up all trash from Boca Beach',
-//     cost: 'Free',
-//     attendees: ['Christian', 'Mia', 'Noah', 'Emily', 'Zack'],
-// }
-
-// function createEvent(){
-//     new Event(newEvent)
-//     .save()
-//     .then(() => console.log('Event was saved'))
-//     .catch(err => console.log(err))
-// }
